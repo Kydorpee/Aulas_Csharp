a96ae0a55394e01cbc29be756b7b4cbe777a8ed5
Pedro Vinicius <lopedro45@gmail.com>
2025-01-30 08:56:19 -0300

    Aula_09

---
+++ Aula_09/Program.cs
@@ -0,0 +1,15 @@
+﻿class Aula09
+{
+    static void Main()
+    {
+        int num = 10;
+        num = num << 1;
+
+        Console.WriteLine(num);
+            /* 
+            << dobrar
+            >> Dividir
+            */
+    }
+}
+
